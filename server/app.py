5054cb79d2b389507ba25090b0d0af492a7ad021
Jay Lim <jlimoman@gmail.com>
2023-06-14 06:38:32 -0600

    complete python-p4-routing-and-views-lab

--- server/app.py
+++ server/app.py
@@ -6,3 +6,47 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+#!/usr/bin/env python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:route>')
+def print_string(route):
+    print(route)
+    return route
+
+@app.route('/count/<int:number>')
+def count(number):
+    count = f''
+    for n in range(number):
+        count += f'{n}\n'
+    return count
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, num2, operation):
+    print(num1, operation, num2)
+    if operation == '+':
+        return str(num1 + num2)
+    
+    elif operation == '-':
+        return str(num1 - num2)
+    
+    elif operation == '*':
+        return str(num1 * num2)
+    
+    elif operation == 'div':
+        return str(num1 / num2)
+    
+    elif operation == '%':
+        return str(num1 % num2)
+
+    return 'Error'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
